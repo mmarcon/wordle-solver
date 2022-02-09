34bf515ccaab2e94deeff404def08ee556e1c24d
Massimiliano Marcon <massimiliano.marcon@mongodb.com>
2022-02-09 08:53:38 +0100

    Build in the right location

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'realm-app/hosting/files/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -64,7 +64,7 @@ export default {
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('realm-app/hosting/files'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
